c5378933d099bce69dfa1a4edf5fdd8b13540cd1
Henrique Landim <henriquelandim@outlook.com.br>
2018-04-24 11:20:57 -0300

    Estrutura e variáveis

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace calccpf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cpfCliente, cpf9, cpf10;
+            int contagem1=10,contagem2=11,resto=0,rs1=0,rs2=0;
+            
+        }
+    }
+}
